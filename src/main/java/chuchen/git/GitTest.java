bb01abec7cece0877fdbece0c43e5ba26e42a96c
chuchen <chuchen@163.com>
2021-10-07 15:40:41 +0800

    first-git commit02

--- src/main/java/chuchen/git/GitTest.java
+++ src/main/java/chuchen/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello!world!");
         System.out.println("hello git!");
         System.out.println("How are you ?");
+        System.out.println("123123");
     }
 }
